6dd024550da9427881b6e6bd3123b8f3da6a307f
Ninad Thakare <ninad.thakare@neosofttech.com>
2017-07-10 15:47:52 +0530

    starting code

---
+++ app/src/main/java/com/neosoft/swipebutton/views/SwipeButton.java
@@ -0,0 +1,216 @@
+package com.neosoft.swipebutton.views;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.neosoft.swipebutton.R;
+
+public class SwipeButton extends RelativeLayout implements View.OnTouchListener {
+
+    private ImageView slidingButton;
+    private float initialX;
+    private boolean active;
+    private int initialButtonWidth;
+    private Drawable disabledDrawable;
+    private Drawable enabledDrawable;
+    private TextView centreText;
+
+
+    public SwipeButton(Context context) {
+        super(context);
+        init(context, null, -1, -1);
+    }
+
+    public SwipeButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, -1, -1);
+    }
+
+    public SwipeButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr, -1);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public SwipeButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        RelativeLayout background = this;
+
+        LayoutParams layoutParamsView = new LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        layoutParamsView.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
+        background.setBackground(ContextCompat.getDrawable(context, R.drawable.shape_rounded));
+
+        /*addView(background, layoutParamsView);*/
+
+        final TextView centerText = new TextView(context);
+        this.centreText = centerText;
+        centerText.setGravity(Gravity.CENTER);
+
+        LayoutParams layoutParams = new LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
+        centerText.setText("SWIPE"); //add any text you need
+        centerText.setTextColor(Color.WHITE);
+
+        background.addView(centerText, layoutParams);
+
+        final ImageView swipeButton = new ImageView(context);
+        this.slidingButton = swipeButton;
+
+        LayoutParams layoutParamsButton = new LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        layoutParamsButton.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+        layoutParamsButton.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
+
+        swipeButton.setBackground(ContextCompat.getDrawable(context, R.drawable.shape_button));
+        disabledDrawable = ContextCompat.getDrawable(context, R.drawable.ic_lock_outline_black_24dp);
+        enabledDrawable = ContextCompat.getDrawable(context, R.drawable.ic_lock_open_black_24dp);
+
+        swipeButton.setImageDrawable(disabledDrawable);
+
+        background.addView(swipeButton, layoutParamsButton);
+
+        setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                if (initialX == 0) {
+                    initialX = slidingButton.getX();
+                }
+                if (event.getX() > initialX + slidingButton.getWidth() / 2 &&
+                        event.getX() + slidingButton.getWidth() / 2 < getWidth()) {
+                    slidingButton.setX(event.getX() - slidingButton.getWidth() / 2);
+                    centreText.setAlpha(1 - 1.3f * ((slidingButton.getX() +
+                            slidingButton.getWidth()) / getWidth()));
+                }
+                if (event.getX() + slidingButton.getWidth() / 2 > getWidth() &&
+                        slidingButton.getX() + slidingButton.getWidth() / 2 < getWidth()) {
+                    slidingButton.setX(getWidth() - slidingButton.getWidth());
+                }
+                if (event.getX() < slidingButton.getWidth() / 2 &&
+                        slidingButton.getX() > 0) {
+                    slidingButton.setX(0);
+                }
+                return true;
+            case MotionEvent.ACTION_UP:
+                if (active) {
+                    collapseButton();
+                } else {
+                    initialButtonWidth = slidingButton.getWidth();
+                    if (slidingButton.getX() + slidingButton.getWidth() > getWidth() * 0.85) {
+                        expandButton();
+                    } else {
+                        moveButtonBack();
+                    }
+                }
+                return true;
+        }
+        return false;
+    }
+
+    private void moveButtonBack() {
+        final ValueAnimator positionAnimator =
+                ValueAnimator.ofFloat(slidingButton.getX(), 0);
+        positionAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        positionAnimator.addUpdateListener(animation -> {
+            float x = (Float) positionAnimator.getAnimatedValue();
+            slidingButton.setX(x);
+        });
+
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(
+                centreText, "alpha", 1);
+
+        positionAnimator.setDuration(200);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(objectAnimator, positionAnimator);
+        animatorSet.start();
+    }
+
+    private void expandButton() {
+        final ValueAnimator positionAnimator = ValueAnimator.ofFloat(slidingButton.getX(), 0);
+        positionAnimator.addUpdateListener(animation -> {
+            float x = (float) positionAnimator.getAnimatedValue();
+            slidingButton.setX(x);
+        });
+
+        final ValueAnimator widthAnimator = ValueAnimator.ofInt(slidingButton.getWidth(), getWidth());
+        widthAnimator.addUpdateListener(animation -> {
+            ViewGroup.LayoutParams params = slidingButton.getLayoutParams();
+            params.width = (int) widthAnimator.getAnimatedValue();
+            slidingButton.setLayoutParams(params);
+        });
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                active = true;
+                slidingButton.setImageDrawable(enabledDrawable);
+            }
+        });
+        animatorSet.playTogether(positionAnimator, widthAnimator);
+        animatorSet.start();
+    }
+
+    private void collapseButton() {
+        final ValueAnimator widthAnimator = ValueAnimator.ofInt(slidingButton.getWidth(),
+                initialButtonWidth);
+        widthAnimator.addUpdateListener(animation -> {
+            ViewGroup.LayoutParams params = slidingButton.getLayoutParams();
+            params.width = (int) widthAnimator.getAnimatedValue();
+            slidingButton.setLayoutParams(params);
+        });
+
+        widthAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                active = false;
+                slidingButton.setImageDrawable(disabledDrawable);
+            }
+        });
+
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(centreText, "alpha", 1);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(objectAnimator, widthAnimator);
+        animatorSet.start();
+    }
+}
